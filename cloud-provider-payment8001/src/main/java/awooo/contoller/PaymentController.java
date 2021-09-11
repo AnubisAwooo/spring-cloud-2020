d10b59d39e7a92d874ba2e22ca3c4b80be51eb22
Dawn <dawn@yinian.tech>
2021-09-11 23:14:30 +0800

    6

--- cloud-provider-payment8001/src/main/java/awooo/contoller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/awooo/contoller/PaymentController.java
@@ -5,6 +5,7 @@ import awooo.entities.Payment;
 import awooo.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -15,14 +16,17 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping("/create")
     public Message<?> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("插入结果 * " + result);
         if (result > 0) {
-            return new Message<>(0, "success", result);
+            return new Message<>(0, "success port: " + serverPort, result);
         } else {
-            return new Message<>(444, "failed", null);
+            return new Message<>(444, "failed port: " + serverPort, null);
         }
     }
 
@@ -31,9 +35,9 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("查询结果 " + payment);
         if (null != payment) {
-            return new Message<>(0, "success", payment);
+            return new Message<>(0, "success port: " + serverPort, payment);
         } else {
-            return new Message<>(444, "failed: " + id, null);
+            return new Message<>(444, "failed: " + id + " port: " + serverPort, null);
         }
     }
 
