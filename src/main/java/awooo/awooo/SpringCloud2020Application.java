7dd4c59500593f635aad0bc2c7297e97712cf0f5
Dawn <dawn@yinian.tech>
2021-09-11 17:36:16 +0800

    init

---
+++ src/main/java/awooo/awooo/SpringCloud2020Application.java
@@ -0,0 +1,13 @@
+package awooo.awooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloud2020Application.class, args);
+    }
+
+}
