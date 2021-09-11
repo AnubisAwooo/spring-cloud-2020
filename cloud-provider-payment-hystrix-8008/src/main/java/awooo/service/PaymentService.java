17e7c7426742710c5bec8dea3ef6f08c32e660d6
Dawn <dawn@yinian.tech>
2021-09-12 03:56:36 +0800

    16

--- cloud-provider-payment-hystrix-8008/src/main/java/awooo/service/PaymentService.java
+++ cloud-provider-payment-hystrix-8008/src/main/java/awooo/service/PaymentService.java
@@ -1,5 +1,7 @@
 package awooo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,14 +11,22 @@ public class PaymentService {
         return "线程池: " + Thread.currentThread().getName() + " " + id + " O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandle", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfoTimeout(Long id) {
-        long timeout = 3000;
-        try {
-            Thread.sleep(timeout);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        long timeout = 5000;
+//        try {
+//            Thread.sleep(timeout);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int age = 10/0;
         return "线程池: " + Thread.currentThread().getName() + " " + id + " O(∩_∩)O哈哈~ timeout: " + timeout + "ms";
+    }
+
+    public String paymentInfoTimeoutHandle(Long id) {
+        return "线程池: " + Thread.currentThread().getName() + " " + id + " o(╥﹏╥)o";
     }
 
 }
