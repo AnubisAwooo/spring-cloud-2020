d5d1556ea959ebae8422761dfad47bfbd8ec01c0
Anubis <anubisawooo@163.com>
2021-09-12 23:49:07 +0800

    21

---
+++ cloud-gateway-gateway-9527/src/main/java/awooo/filter/MyLogGatewayFilter.java
@@ -0,0 +1,36 @@
+package awooo.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info(getClass().getName() + " " + new Date());
+
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (null == username) {
+            log.error("no username");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
